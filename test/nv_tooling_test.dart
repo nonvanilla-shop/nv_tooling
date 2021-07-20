347f14c0ac7fdb7e083c0ff2be6c697171bac11a
Marco Papula <marco.papula@nonvanilla.shop>
2021-07-20 15:57:49 +0200

    fix and extend tests

--- test/nv_tooling_test.dart
+++ test/nv_tooling_test.dart
@@ -9,7 +9,7 @@ void main() {
     expect(success.isSuccess, true);
   });
 
-  test('Success has boolean getters with correct return values', () {
+  test('Failure has boolean getters with correct return values', () {
     final success = failureOf(12);
     expect(success.isFailure, true);
     expect(success.isSuccess, false);
@@ -17,11 +17,45 @@ void main() {
 
   test('Success accessor calls onSuccess function', () {
     final SuccessOrFailure<int, bool> success = successOf(12);
-    expect(success.access((success) => 12, (failure) => -1), 12);
+    expect(success.access((success) => success, (failure) => -1), 12);
   });
 
   test('Failure accessor calls onFailure function', () {
+    final SuccessOrFailure<int, bool> failure = failureOf(true);
+    expect(failure.access((success) => false, (failure) => failure), true);
+  });
+
+  test('Failure maps to new failure correctly', () {
+    final SuccessOrFailure<int, bool> failure = failureOf(true);
+    expect(
+      failure
+          .map(failure: (f) => -1)
+          .access((success) => 12, (failure) => failure),
+      -1,
+    );
+  });
+
+  test('Failure maps to identity if no param passed', () {
+    final SuccessOrFailure<int, bool> failure = failureOf(true);
+    expect(
+      failure.map().access((success) => false, (failure) => failure),
+      true,
+    );
+  });
+
+  test('Success maps to new success correctly', () {
     final SuccessOrFailure<int, bool> success = successOf(12);
-    expect(success.access((success) => 12, (failure) => -1), -1);
+    expect(
+      success.map(success: (s) => 'value: $s').access(
+            (success) => success,
+            (failure) => failure,
+          ),
+      'value: 12',
+    );
+  });
+
+  test('Failure maps to identity if no param passed', () {
+    final SuccessOrFailure<int, bool> success = successOf(1);
+    expect(success.map().access((success) => success, (failure) => failure), 1);
   });
 }
