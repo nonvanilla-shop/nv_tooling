2872c993b1713c93a0b164b869148d28a7801ed4
Marco Papula <marco.papula@nonvanilla.shop>
2021-07-21 11:33:45 +0200

    add selection class

--- lib/nv_tooling.dart
+++ lib/nv_tooling.dart
@@ -1,5 +1,5 @@
 library nv_tooling;
 
 export 'nv_tooling/success_or_failure.dart';
-export 'nv_tooling/nothing.dart';
+export 'nv_tooling/selection.dart';
 export 'nv_tooling/iterable_extensions.dart';
---
+++ lib/nv_tooling/selection.dart
@@ -0,0 +1,54 @@
+import 'package:collection/collection.dart';
+
+abstract class _Selection<T> {
+  final List<T> _options;
+  final T? _selected;
+
+  _Selection(this._options, this._selected)
+      : assert((_selected != null) == _options.contains(_selected));
+
+  @override
+  bool operator ==(Object other) =>
+      other is _Selection &&
+      ListEquality().equals(_options, other._options) &&
+      _selected == other._selected;
+
+  @override
+  int get hashCode => _options.hashCode + _selected.hashCode;
+
+  _Selection copyWith(T selection);
+}
+
+class Selection<T> extends _Selection<T> {
+  List<T> get options => _options;
+  T get selected => _selected!;
+
+  Selection({
+    required List<T> options,
+    required T selected,
+  }) : super(options, selected);
+
+  @override
+  Selection<T> copyWith(T selection) => Selection(
+        options: options,
+        selected: selection,
+      );
+}
+
+class OptionalSelection<T> extends _Selection<T> {
+  List<T> get options => _options;
+  T? get selected => _selected;
+
+  OptionalSelection({
+    required List<T> options,
+    T? selected,
+  }) : super(options, selected);
+
+  @override
+  OptionalSelection<T> copyWith(T? selection) => OptionalSelection(
+        options: options,
+        selected: selection,
+      );
+
+  bool get hasSelection => selected != null;
+}
